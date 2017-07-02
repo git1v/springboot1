0198de72da0fb12ff8f30c24ee0b7a6048f3950e
dean <git1v@yahoo.co.jp>
2017-07-02 12:44:29 +0900

    input data in Controller

--- src/main/java/com/rose/web/HelloController.java
+++ src/main/java/com/rose/web/HelloController.java
@@ -1,13 +1,16 @@
 package com.rose.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
 	@RequestMapping("/")
-	public String index(){
+	public String index(Model model){
+		model.addAttribute("title", "Practice Page");
+		model.addAttribute("message","this is made in Controller");
 		return "helo";
 	}
 }
