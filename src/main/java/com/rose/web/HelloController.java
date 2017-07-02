afe67fbf335dac462ab1a93c901b7bf0df2fd500
dean <git1v@yahoo.co.jp>
2017-07-02 20:37:02 +0900

    use Bean

--- src/main/java/com/rose/web/HelloController.java
+++ src/main/java/com/rose/web/HelloController.java
@@ -1,14 +1,16 @@
 package com.rose.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HelloController {
+	@Autowired
+	MySpringBean bean;
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String index(Model model){
 		model.addAttribute("title", "Practice Page");
@@ -17,10 +19,10 @@ public class HelloController {
 	}
 	
     @RequestMapping(value="/", method=RequestMethod.POST)
-    public String form(@RequestParam("input1")String input1, Model model){
-        String res="あなたは「"+input1+"」と入力しました。";
-        model.addAttribute("title", "Answer Page");
-        model.addAttribute("message", res);
+    public String form(@RequestParam("input1")int input1, Model model){
+    	MyData data=bean.get(input1);
+    	model.addAttribute("title", "Answer Page");
+        model.addAttribute("message", data);
         return "helo";
     }
 }
