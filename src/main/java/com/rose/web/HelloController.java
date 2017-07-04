33d9297523963963ba35791512a51d3ea7c85a58
dean <git1v@yahoo.co.jp>
2017-07-04 21:10:56 +0900

    made some forms

--- src/main/java/com/rose/web/HelloController.java
+++ src/main/java/com/rose/web/HelloController.java
@@ -1,16 +1,11 @@
 package com.rose.web;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
-import com.rose.web.MyDataEntity;
-import com.rose.web.MyDataEntityRepository;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -18,13 +13,39 @@ public class HelloController {
 	MyDataEntityRepository repository;
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
-	public String index(Model model){
-		model.addAttribute("title","Helo Page");
-		model.addAttribute("message", "MyDataEntity List.");
-		List<MyDataEntity> data=repository.findAll();
-		model.addAttribute("data",data);
-		return "helo";
+	public ModelAndView index(ModelAndView mav){
+		mav.setViewName("index");
+		mav.addObject("msg","フォームを送信して下さい。");
+		return mav;
 	}
 	
+	@RequestMapping(value="/", method=RequestMethod.POST)
+	public ModelAndView send(
+			@RequestParam(value="check1", required=false)boolean check1,
+			@RequestParam(value="radio1", required=false)String radio1,
+			@RequestParam(value="select1", required=false)String select1,
+			@RequestParam(value="select2", required=false)String[] select2,
+			ModelAndView mav
+			){
+		String res="";
+		try {
+			res="check: "+check1+
+				" radio: "+radio1+
+				" select: "+select1+
+				" \n select2: ";
+		} catch (NullPointerException e) {}
+		
+		try {
+			res+=select2[0];
+			for(int i=1;i<select2.length;i++){
+				res+=" , "+select2[i];
+			}
+		} catch (NullPointerException e) {
+			res+="null";
+		}
+		mav.addObject("msg", res);
+		mav.setViewName("index");
+		return mav;
+	}
 
 }
